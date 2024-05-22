bb4b25e140346849c1d10e9eb71094d1bf84d7f1
Seb <sebheron21@hotmail.co.uk>
2024-05-22 14:20:08 +0100

    Remove second canvas from screen gizmo

--- src/components/Gizmos/ScreenGizmo/ScreenGizmo.tsx
+++ src/components/Gizmos/ScreenGizmo/ScreenGizmo.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { Illustration, Shape } from "react-zdog";
 import { TAU } from "zdog";
 import useCamera from "@/components/Camera/useCamera";
 import Dolly from "@/components/Dolly/Dolly";
+import Colors from "@/constants/Colors";
 import VectorType from "@/types/VectorType";
 import styles from "./ScreenGizmo.module.css";
 
@@ -16,37 +17,37 @@ type GizmoBall = {
 const CAPS: GizmoBall[] = [
   { color: "#7c59ae" },
   {
-    color: "#f00",
+    color: Colors.XAXIS,
     offset: { x: 30, y: 0, z: 0 },
     targetRotation: { x: 0, y: TAU / 4, z: 0 },
     targetPosition: { x: 30, y: 0, z: 0 },
   },
   {
-    color: "#505050",
+    color: Colors.GRID,
     offset: { x: -30, y: 0, z: 0 },
     targetRotation: { x: 0, y: -TAU / 4, z: 0 },
     targetPosition: { x: -30, y: 0, z: 0 },
   },
   {
-    color: "#0f0",
+    color: Colors.YAXIS,
     offset: { x: 0, y: -30, z: 0 },
     targetRotation: { x: -TAU / 4, y: 0, z: 0 },
     targetPosition: { x: 0, y: -30, z: 0 },
   },
   {
-    color: "#505050",
+    color: Colors.GRID,
     offset: { x: 0, y: 30, z: 0 },
     targetRotation: { x: TAU / 4, y: 0, z: 0 },
     targetPosition: { x: 0, y: 30, z: 0 },
   },
   {
-    color: "#4c41c8",
+    color: Colors.ZAXIS,
     offset: { x: 0, y: 0, z: 30 },
     targetRotation: { x: 0, y: 0, z: 0 },
     targetPosition: { x: 0, y: 0, z: 30 },
   },
   {
-    color: "#505050",
+    color: Colors.GRID,
     offset: { x: 0, y: 0, z: -30 },
     targetRotation: { x: 0, y: -TAU / 2, z: 0 },
     targetPosition: { x: 0, y: 0, z: -30 },
@@ -59,29 +60,21 @@ const Gizmo = () => {
 
   return (
     <div className={styles.gizmo}>
-      <Illustration className={styles.illo} element="canvas">
+      <Illustration className={styles.illo} element="canvas" pointerEvents>
         <Dolly canRotate>
           <Shape stroke={2} color={"#f00"} path={[{}, { x: 30 }]} />
           <Shape stroke={2} color={"#0f0"} path={[{}, { y: -30 }]} />
           <Shape stroke={2} color={"#4c41c8"} path={[{}, { z: 30 }]} />
           {CAPS.map((cap, index) => (
-            <Shape
-              key={index}
-              stroke={hoverIndex === index ? 14 : 10}
-              color={cap.color}
-              translate={cap.offset}
-            />
-          ))}
-        </Dolly>
-      </Illustration>
-      <Illustration className={styles.hidden} pointerEvents element="canvas">
-        <Dolly canRotate>
-          {CAPS.map(
-            (cap, index) =>
-              !!cap.targetRotation &&
-              !!cap.targetPosition && (
+            <Fragment key={index}>
+              <Shape
+                stroke={hoverIndex === index ? 14 : 10}
+                color={cap.color}
+                translate={cap.offset}
+              />
+              {!!cap.targetRotation && !!cap.targetPosition && (
                 <Shape
-                  key={index}
+                  color="#ffffff00"
                   stroke={20}
                   translate={cap.offset}
                   onPointerEnter={() => setHoverIndex(index)}
@@ -90,8 +83,9 @@ const Gizmo = () => {
                     lerpTo(cap.targetPosition!, cap.targetRotation!, 1000)
                   }
                 />
-              ),
-          )}
+              )}
+            </Fragment>
+          ))}
         </Dolly>
       </Illustration>
     </div>
