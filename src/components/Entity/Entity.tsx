c249262f7ccbae641533669a6cd7d509fe1afb82
Seb <sebheron21@hotmail.co.uk>
2024-05-22 14:23:23 +0100

    Selection cover for entity

--- src/components/Entity/Entity.tsx
+++ src/components/Entity/Entity.tsx
@@ -1,3 +1,5 @@
+import { Fragment, useCallback } from "react";
+import useScene from "@/components/Scene/useScene";
 import EntityType from "@/types/EntityType";
 
 interface Props {
@@ -5,11 +7,36 @@ interface Props {
 }
 
 const Entity = ({ entities }: Props) => {
-  return entities.map(({ id, component: Component, props, children }) => {
+  const { selected, select } = useScene();
+
+  const handleClick = useCallback(
+    (entity: EntityType) => {
+      select((prev) => (prev?.id === entity.id ? null : entity));
+    },
+    [select],
+  );
+
+  return entities.map((entity) => {
+    const highlightProps = {
+      stroke: ((entity.props.stroke as number) ?? 1) + 5,
+      color: "#6d59ff45",
+      fill: false,
+      closed: true,
+    };
+
     return (
-      <Component key={id} {...props}>
-        {children && <Entity entities={children} />}
-      </Component>
+      <Fragment key={entity.id}>
+        <entity.component {...entity.props} onClick={() => handleClick(entity)}>
+          {entity.children && <Entity entities={entity.children} />}
+        </entity.component>
+        {selected?.id === entity.id && (
+          <entity.component
+            {...entity.props}
+            {...highlightProps}
+            onClick={() => select(null)}
+          />
+        )}
+      </Fragment>
     );
   });
 };
