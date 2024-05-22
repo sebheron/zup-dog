aa74ec0395861740a33ea20d1a5d95a396d6255f
Seb <sebheron21@hotmail.co.uk>
2024-05-22 14:24:04 +0100

    Better entity type

--- src/types/EntityType.ts
+++ src/types/EntityType.ts
@@ -1,9 +1,16 @@
 import { FC, PropsWithChildren } from "react";
+import { AnchorOptions } from "zdog";
 import MustDeclare from "./MustDeclare";
 
+interface BaseType extends PropsWithChildren {
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+  onPointerEnter?: () => void;
+  onPointerLeave?: () => void;
+}
+
 type EntityType<
-  C extends FC<PropsWithChildren> = FC<PropsWithChildren>,
-  O extends object = object,
+  C extends FC<BaseType> = FC<BaseType>,
+  O extends object = Record<string, unknown> & AnchorOptions,
 > = {
   id: string;
   name: string;
