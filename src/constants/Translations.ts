147d96e5de688cfe0a939697017a9849906d4772
Seb <sebheron21@hotmail.co.uk>
2024-05-24 18:49:31 +0100

    Rotations and translations

--- src/constants/Translations.ts
+++ src/constants/Translations.ts
@@ -1,21 +1,21 @@
 import { TAU } from "zdog";
-import GizmoType from "@/types/GizmoType";
 import Colors from "./Colors";
 
-const Translations: Record<string, GizmoType> = {
-  X: {
+const Translations = {
+  XT: {
     color: Colors.XAXIS,
     direction: { x: 1, y: 0, z: 0 },
     rotation: { y: -TAU / 4 },
   },
-  Y: {
+  YT: {
     color: Colors.YAXIS,
     direction: { x: 0, y: -1, z: 0 },
     rotation: { x: TAU / 4 },
   },
-  Z: {
+  ZT: {
     color: Colors.ZAXIS,
     direction: { x: 0, y: 0, z: 1 },
+    rotation: {},
   },
 };
 
