219ef207ddaa76cc670cdddb153a592a5f96ebc5
Seb <sebheron21@hotmail.co.uk>
2024-05-24 01:28:47 +0100

    Use new dolly

--- src/components/ScreenGizmo/ScreenGizmo.tsx
+++ src/components/ScreenGizmo/ScreenGizmo.tsx
@@ -1,48 +1,50 @@
 import { Fragment, useState } from "react";
 import { Illustration, Shape } from "react-zdog-alt";
 import useCamera from "@/components/Camera/useCamera";
-import Dolly from "@/components/Dolly/Dolly";
 import CAPS, { Cap } from "@/constants/Caps";
 import styles from "./ScreenGizmo.module.css";
 
 const Gizmo = () => {
-  const { lerpTo } = useCamera();
+  const { lerpTo, rotation } = useCamera();
   const [interactable, setInteractable] = useState<Cap | null>(null);
 
   return (
     <div className={styles.gizmo}>
-      <Illustration className={styles.illo} element="canvas" pointerEvents>
-        <Dolly canRotate>
-          {Object.entries(CAPS).map(([key, cap]) => (
-            <Fragment key={key}>
+      <Illustration
+        className={styles.illo}
+        element="canvas"
+        rotate={rotation}
+        pointerEvents
+      >
+        {Object.entries(CAPS).map(([key, cap]) => (
+          <Fragment key={key}>
+            <Shape
+              stroke={2}
+              color={cap.color}
+              path={[{}, cap.offset!]}
+              pointerEvents={false}
+            />
+            <Shape
+              stroke={interactable === key ? 14 : 10}
+              color={cap.color}
+              translate={cap.offset}
+              pointerEvents={false}
+            />
+            {!!cap.targetRotation && !!cap.targetPosition && (
               <Shape
-                stroke={2}
-                color={cap.color}
-                path={[{}, cap.offset!]}
-                pointerEvents={false}
-              />
-              <Shape
-                stroke={interactable === key ? 14 : 10}
-                color={cap.color}
+                stroke={20}
                 translate={cap.offset}
-                pointerEvents={false}
+                onPointerMove={() => setInteractable(key)}
+                onPointerLeave={() => setInteractable(null)}
+                onClick={() =>
+                  lerpTo(cap.targetPosition!, cap.targetRotation!, 1000)
+                }
+                visible={false}
+                pointerEvents
               />
-              {!!cap.targetRotation && !!cap.targetPosition && (
-                <Shape
-                  stroke={20}
-                  translate={cap.offset}
-                  onPointerMove={() => setInteractable(key)}
-                  onPointerLeave={() => setInteractable(null)}
-                  onClick={() =>
-                    lerpTo(cap.targetPosition!, cap.targetRotation!, 1000)
-                  }
-                  visible={false}
-                  pointerEvents
-                />
-              )}
-            </Fragment>
-          ))}
-        </Dolly>
+            )}
+          </Fragment>
+        ))}
       </Illustration>
     </div>
   );
