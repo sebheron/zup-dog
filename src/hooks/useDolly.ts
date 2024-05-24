484aad390fc34554798666c17914b983db0f7cc0
Seb <sebheron21@hotmail.co.uk>
2024-05-24 01:28:18 +0100

    Dolly now hook

---
+++ src/hooks/useDolly.ts
@@ -0,0 +1,63 @@
+import { useCallback } from "react";
+import useCamera from "@/components/Camera/useCamera";
+import vector from "@/utils/vector";
+
+const useDolly = () => {
+  const { zoom, setPosition, setRotation, setZoom } = useCamera();
+
+  const handleZoom = useCallback(
+    (event: WheelEvent) => {
+      if (event.buttons) return;
+      setZoom((prevZoom) =>
+        Math.max(0.1, Math.min(prevZoom - event.deltaY / 1000, 5)),
+      );
+    },
+    [setZoom],
+  );
+
+  const handleTranslate = useCallback(
+    (event: MouseEvent) => {
+      event.preventDefault();
+      setPosition((position) =>
+        vector.shift(
+          position,
+          event.movementX * 0.8 * (1 / zoom),
+          event.movementY * 0.8 * (1 / zoom),
+        ),
+      );
+    },
+    [zoom, setPosition],
+  );
+
+  const handleRotate = useCallback(
+    (event: MouseEvent) => {
+      event.preventDefault();
+      setRotation((rotation) =>
+        vector.spin(rotation, event.movementX / 600, event.movementY / 600),
+      );
+    },
+    [setRotation],
+  );
+
+  const handleMove = useCallback(
+    (event: MouseEvent) => {
+      if (event.buttons === 2) handleTranslate(event);
+      else if (event.buttons === 4) handleRotate(event);
+    },
+    [handleTranslate, handleRotate],
+  );
+
+  const handleContextMenu = useCallback((event: MouseEvent) => {
+    event.preventDefault();
+  }, []);
+
+  const register = () => ({
+    onWheel: handleZoom,
+    onMouseMove: handleMove,
+    onContextMenu: handleContextMenu,
+  });
+
+  return { register };
+};
+
+export default useDolly;
