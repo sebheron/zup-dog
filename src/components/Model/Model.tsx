36d3a15ac994fb3d98912054be2a6fc79ca4b8b6
Seb <sebheron21@hotmail.co.uk>
2024-05-30 21:38:39 +0100

    Remove selected behaviour from model

--- src/components/Model/Model.tsx
+++ src/components/Model/Model.tsx
@@ -1,5 +1,4 @@
-import { Fragment, useCallback } from "react";
-import useScene from "@/components/Scene/useScene";
+import { Fragment } from "react";
 import Components from "@/constants/Components";
 import InstanceType from "@/types/InstanceType";
 
@@ -8,44 +7,16 @@ interface Props {
   onClick: (instance: InstanceType) => void;
 }
 
-const Model = ({ objects, onClick }: Props) => {
-  const { selected } = useScene();
-
-  const getSelectedProps = useCallback(
-    (obj: InstanceType) => ({
-      stroke: ((obj.props.stroke as number) ?? 1) + 4,
-      color: "black",
-      fill: false,
-      frontFace: true,
-      rearFace: true,
-      leftFace: true,
-      rightFace: true,
-      topFace: true,
-      bottomFace: true,
-      backface: true,
-    }),
-    [selected],
-  );
-
-  return objects.map((obj) => {
+const Model = ({ objects, onClick }: Props) =>
+  objects.map((obj) => {
     const Component = Components[obj.shape];
-    const selectedProps = getSelectedProps(obj);
-
     return (
       <Fragment key={obj.id}>
         <Component {...obj.props} onClick={() => onClick(obj)}>
           {obj.children && <Model objects={obj.children} onClick={onClick} />}
         </Component>
-        {selected?.id === obj.id && (
-          <Component
-            {...obj.props}
-            {...selectedProps}
-            onClick={() => onClick(obj)}
-          />
-        )}
       </Fragment>
     );
   });
-};
 
 export default Model;
