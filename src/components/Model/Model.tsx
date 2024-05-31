39109e7f978eb358b4a6a488f6a5ac54a5ff951c
Seb <sebheron21@hotmail.co.uk>
2024-05-31 17:47:16 +0100

    Stop propagation on model mouse down

--- src/components/Model/Model.tsx
+++ src/components/Model/Model.tsx
@@ -1,22 +1,39 @@
-import { Fragment } from "react";
+import { Fragment, useCallback } from "react";
 import Components from "@/constants/Components";
 import InstanceType from "@/types/InstanceType";
 
 interface Props {
   objects: InstanceType[];
+  hide?: unknown;
   onClick: (instance: InstanceType) => void;
 }
 
-const Model = ({ objects, onClick }: Props) =>
-  objects.map((obj) => {
+const Model = ({ objects, hide, onClick }: Props) => {
+  const handleModelClick = useCallback(
+    (e: React.MouseEvent<HTMLElement, MouseEvent>, instance: InstanceType) => {
+      e.stopPropagation();
+      onClick(instance);
+    },
+    [onClick],
+  );
+
+  return objects.map((obj) => {
     const Component = Components[obj.shape];
+    const props = { ...obj.props, visible: obj.props.visible && !hide };
     return (
       <Fragment key={obj.id}>
-        <Component {...obj.props} onClick={() => onClick(obj)}>
-          {obj.children && <Model objects={obj.children} onClick={onClick} />}
+        <Component {...props} onClick={(e) => handleModelClick(e, obj)}>
+          {obj.children && (
+            <Model
+              objects={obj.children}
+              onClick={onClick}
+              hide={hide || !obj.props.visible}
+            />
+          )}
         </Component>
       </Fragment>
     );
   });
+};
 
 export default Model;
