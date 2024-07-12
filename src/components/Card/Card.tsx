cdae145426c1db95f250313f50fbbd44a9fa8a57
Seb <sebheron21@hotmail.co.uk>
2024-07-12 01:46:31 +0100

    Allow div props on card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,14 @@ import clsx from "clsx";
 import { PropsWithChildren } from "react";
 import styles from "./Card.module.css";
 
-interface Props extends PropsWithChildren {
+interface Props
+  extends PropsWithChildren,
+    Omit<React.HTMLAttributes<HTMLDivElement>, "className"> {
   position: "left" | "right";
 }
 
-const Card = ({ children, position }: Props) => (
-  <div className={clsx(styles.card, styles[position])}>
+const Card = ({ children, position, ...props }: Props) => (
+  <div className={clsx(styles.card, styles[position])} {...props}>
     <div className={styles.container}>{children}</div>
   </div>
 );
