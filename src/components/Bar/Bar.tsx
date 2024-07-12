39d877b64d580f853598e82446fc25ab031e864a
Seb <sebheron21@hotmail.co.uk>
2024-07-12 03:53:38 +0100

    load/save basic implementation

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BiExport, BiSolidCameraMovie } from "react-icons/bi";
+import { BiExport, BiImport, BiSave, BiSolidCameraMovie } from "react-icons/bi";
 import { PiPaintBrushFill } from "react-icons/pi";
 import { TbCube3dSphere } from "react-icons/tb";
 import Button from "@/components/Button/Button";
@@ -14,13 +14,65 @@ import Icons from "@/constants/Icons";
 import Objects from "@/constants/Objects";
 import ToolType from "@/types/ToolType";
 import tooltip from "@/utils/tooltip";
+import Load from "../Button/Load";
 import styles from "./Bar.module.css";
 
 const Bar = () => {
   const [exporting, setExporting] = useState(false);
-  const { tool, paintColor, selected, add, setPaintColor, setTool } =
-    useScene();
-  const { reset } = useCamera();
+  const {
+    tool,
+    paintColor,
+    selected,
+    objects,
+    add,
+    setPaintColor,
+    setTool,
+    setObjects,
+  } = useScene();
+  const { position, rotation, zoom, setPosition, setRotation, setZoom, reset } =
+    useCamera();
+
+  const saveToFile = () => {
+    const link = document.createElement("a");
+    link.download = "scene.json";
+    link.href = URL.createObjectURL(
+      new Blob(
+        [
+          JSON.stringify(
+            {
+              objects,
+              camera: {
+                position,
+                rotation,
+                zoom,
+              },
+            },
+            null,
+            2,
+          ),
+        ],
+        {
+          type: "application/json",
+        },
+      ),
+    );
+    link.click();
+  };
+
+  const loadFromFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const { objects, camera } = JSON.parse(e.target?.result as string);
+        setObjects(objects);
+        setPosition(camera.position);
+        setRotation(camera.rotation);
+        setZoom(camera.zoom);
+      };
+      reader.readAsText(file);
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -63,6 +115,16 @@ const Bar = () => {
         >
           <Menu.Item onClick={reset}>Reset Camera</Menu.Item>
         </Menu>
+        <Load
+          onChange={loadFromFile}
+          {...tooltip("Load")}
+          accept="application/json"
+        >
+          <BiImport />
+        </Load>
+        <Button onClick={() => saveToFile()} {...tooltip("Save")}>
+          <BiSave />
+        </Button>
         <Button onClick={() => setExporting(true)} {...tooltip("Export")}>
           <BiExport />
         </Button>
