f6e3f6cadf318131999cb59506368b78180f4369
Seb <sebheron21@hotmail.co.uk>
2024-07-28 17:58:56 +0100

    Fixed build errors

--- src/components/Model/Model.tsx
+++ src/components/Model/Model.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import Components from "@/constants/Components";
 import InstanceType from "@/types/InstanceType";
 
